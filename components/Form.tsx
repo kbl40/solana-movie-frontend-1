f3eddfe90ea65574f00dbc9127556885817d513f
Kyle B. Lackinger <67078189+kbl40@users.noreply.github.com>
2022-09-22 09:54:29 -0400

    updated program id

--- components/Form.tsx
+++ components/Form.tsx
@@ -18,7 +18,7 @@ import {
 import * as web3 from "@solana/web3.js"
 import { useConnection, useWallet } from "@solana/wallet-adapter-react"
 
-const MOVIE_REVIEW_PROGRAM_ID = "BNU4WMofFddN8wTKGSm67wapnHfnBqx8BQDZswZwZTf3"
+const MOVIE_REVIEW_PROGRAM_ID = "Ei8Cx9wMHmzmzo1bUuh2PmoTkyhTptdsnF9tiea2QJVv"
 
 export const Form: FC = () => {
     const [title, setTitle] = useState("")
--- coordinators/MovieCoordinator.ts
+++ coordinators/MovieCoordinator.ts
@@ -2,7 +2,7 @@ import bs58 from "bs58"
 import * as web3 from "@solana/web3.js"
 import { Movie } from "../models/Movie"
 
-const MOVIE_REVIEW_PROGRAM_ID = "BNU4WMofFddN8wTKGSm67wapnHfnBqx8BQDZswZwZTf3"
+const MOVIE_REVIEW_PROGRAM_ID = "Ei8Cx9wMHmzmzo1bUuh2PmoTkyhTptdsnF9tiea2QJVv"
 
 export class MovieCoordinator {
     static accounts: web3.PublicKey[] = []
